b1a4b2f77b9ab54f5c1e8ddd357b8f9e9a35162d
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-07-22 20:39:01 +0300

    Добавлена приемка сообщения

---
+++ EmailSender/ResourceModels/NewMailModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EmailSender.ResourceModels
+{
+    public class NewMailModel
+    {
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public string[] Recipients { get; set; }
+
+        public string MailFrom { get; set; }
+    }
+}
