a05c66945d55b18a67d8f058b3fad3db60852a12
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-07-23 16:11:56 +0300

    Добавлена новая миграция

--- EmailSender/Controllers/MailController.cs
+++ EmailSender/Controllers/MailController.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using EmailSender.Data.Infrastructure;
 using EmailSender.Data.Repositories.Interfaces;
